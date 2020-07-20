51674d227475d70b567b0f9f8edec154570baca0
gigajew <ragnarolsen96@gmail.com>
2020-07-19 22:40:39 -0500

    added icon

--- fakemic/Properties/AssemblyInfo.cs
+++ fakemic/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("fakemic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("fakemic by gigajew")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("fakemic")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © gigajew 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
